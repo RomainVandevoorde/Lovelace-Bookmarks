dac8d6e432be6148c18bb5f4a446fd10d0c05068
Romain Vandevoorde <romain.vandevoorde.dev@gmail.com>
2018-06-28 14:50:39 +0200

    add display bookmark

--- bookmark.php
+++ bookmark.php
@@ -6,16 +6,33 @@ if(isset($_GET['action'])) {
 
   switch ($_GET['action']) {
     case 'add':
-
       echo $twig->render('form-bookmark-add.html');
-
       break;
-
     default:
-      echo 'Tu veux faire quoi wesh ?';
+      echo 'Invalid action';
       break;
   }
 
+}
+
+elseif(isset($_GET['id'])) {
+
+  require_once 'db.php';
+
+  $bookmark_id = (int) $_GET['id'];
+
+  if($bookmark_id < 0) exit('invalid id');
+
+  $req = $bdd->prepare("SELECT * FROM bookmarks WHERE id = ?");
+  $req->execute(array($bookmark_id));
+  $data = $req->fetch(PDO::FETCH_ASSOC);
+
+  echo $twig->render('bookmark-display.html', $data);
+
+}
+
+else {
+  exit ('Invalid action');
 }
 
 ?>
