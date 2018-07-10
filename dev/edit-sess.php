dc687ab59a9c7e5f1f43ab0593dfb05339f7ae42
Romain Vandevoorde <romain.vandevoorde.dev@gmail.com>
2018-07-10 12:29:02 +0200

    add user id get parameter

--- dev/edit-sess.php
+++ dev/edit-sess.php
@@ -2,16 +2,12 @@
 
 session_start();
 
-$_SESSION['user_id'] = 1;
-
-if(isset($_GET['rights']) && intval($_GET['rights']) >= 0) {
-  $_SESSION['rights'] = $_GET['rights'];
-}
-else $_SESSION['rights'] = 1;
+$_SESSION['user_id'] = isset($_GET['u']) ? intval($_GET['u']) : 1;
+$_SESSION['rights'] = isset($_GET['r']) ? intval($_GET['r']) : 1;
 
 echo '<p><a href="./">dev folder</a></p>';
 echo '<p><a href="./dump-sess.php">dump sess</a></p>';
 
-echo '<p>Session created with rights level <b>'.$_SESSION['rights'].'</b></p>';
+echo '<p>Session created with user ID <b>'.$_SESSION['user_id'].'</b> and rights level <b>'.$_SESSION['rights'].'</b></p>';
 
 ?>
