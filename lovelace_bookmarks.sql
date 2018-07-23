78468b29c95a61761498b01b028243d383f215a1
Romain Vandevoorde <romain.vandevoorde.dev@gmail.com>
2018-07-23 10:02:55 +0200

    SQL management done

--- lovelace_bookmarks.sql
+++ lovelace_bookmarks.sql
@@ -1,116 +1,114 @@
--- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.7.22, for Linux (x86_64)
 --
--- Client :  localhost
--- Généré le :  Ven 22 Juin 2018 à 16:41
--- Version du serveur :  5.7.22-0ubuntu0.16.04.1
--- Version de PHP :  7.0.30-0ubuntu0.16.04.1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: lovelace_bookmarks
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.1.33-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Base de données :  `lovelace_bookmarks`
+-- Current Database: `lovelace_bookmarks`
 --
 
--- --------------------------------------------------------
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `lovelace_bookmarks` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+
+USE `lovelace_bookmarks`;
 
 --
--- Structure de la table `bookmarks`
+-- Table structure for table `bookmarks`
 --
 
+DROP TABLE IF EXISTS `bookmarks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `bookmarks` (
-  `id` smallint(6) NOT NULL,
-  `titre` varchar(50) NOT NULL,
-  `url` text NOT NULL,
-  `description` text NOT NULL,
+  `id` smallint(6) NOT NULL AUTO_INCREMENT,
+  `titre` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `url` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `user_added` smallint(6) NOT NULL,
-  `category_id` tinyint(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `category_id` tinyint(4) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `category_id` (`category_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Contenu de la table `bookmarks`
+-- Dumping data for table `bookmarks`
 --
 
-INSERT INTO `bookmarks` (`id`, `titre`, `url`, `description`, `user_added`, `category_id`) VALUES
-(1, 'Scotch.io', 'http://scotch.io', 'pouet pouet', 1, 1),
-(2, 'pouet', 'pouet.com', 'Lorem', 2, 1),
-(3, 'titre', 'dumdum.be', '', 3, 3);
-
--- --------------------------------------------------------
+LOCK TABLES `bookmarks` WRITE;
+/*!40000 ALTER TABLE `bookmarks` DISABLE KEYS */;
+INSERT INTO `bookmarks` VALUES (1,'Scotch.io','http://scotch.io','pouet pouet tagada\r\ntsoin tsoin prout',1,1),(2,'pouet','pouet.com','Lorem',2,1),(3,'titre','dumdum.be','dum ? dum !',3,3),(4,'CSS Tricks','css-tricks.com','fyudsqfqifiu pdqiufudsfkysqiufodsf',1,1),(10,'Youtube','https://youtube.com','',1,1),(11,'lodswjfldsj','https://prout.com','',1,1);
+/*!40000 ALTER TABLE `bookmarks` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Structure de la table `categories`
+-- Table structure for table `categories`
 --
 
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `categories` (
-  `id` int(11) NOT NULL,
-  `name` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
+  `id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
+  `name` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Structure de la table `users`
+-- Dumping data for table `categories`
 --
 
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` VALUES (1,'Unsorted');
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `firstname` text NOT NULL,
-  `lastname` text NOT NULL,
-  `github-id` text NOT NULL,
-  `rights` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `github_displayName` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `github_id` int(10) unsigned NOT NULL,
+  `rights` tinyint(3) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Index pour les tables exportées
+-- Dumping data for table `users`
 --
 
---
--- Index pour la table `bookmarks`
---
-ALTER TABLE `bookmarks`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `category_id` (`category_id`);
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (2,'RomainVandevoorde',38908146,0);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Index pour la table `categories`
---
-ALTER TABLE `categories`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT pour les tables exportées
---
-
---
--- AUTO_INCREMENT pour la table `bookmarks`
---
-ALTER TABLE `bookmarks`
-  MODIFY `id` smallint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT pour la table `categories`
---
-ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT pour la table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-07-20 15:38:16
